413f9cb799230bbdeba52b924bf6362077196fbe
Sammuel Victor <samvss13@gmail.com>
2026-05-08 15:16:18 +0000

    1216

--- lista2/Main.java
+++ lista2/Main.java
@@ -13,7 +13,7 @@ public class Main {
             System.out.println("1. Add Cat | 2. Listar | 3. Remover | 4. Som | 5. Habilidade | 6. Sair");
             opcao = ler.nextInt();
 
-            if (opcao == 1) { // Adicionar [cite: 35]
+            if (opcao == 1) { // Adicionar
                 System.out.print("ID: ");
                 long id = ler.nextLong();
                 System.out.print("Idade: ");
@@ -21,11 +21,11 @@ public class Main {
                 System.out.print("Peso: ");
                 double peso = ler.nextDouble();
                 meuZoo.addAnimal(new Cat(id, idade, peso)); // Exemplo com Cat
-            } else if (opcao == 2) { // Listar [cite: 36]
+            } else if (opcao == 2) { // Listar
                 for (Animal a : meuZoo.listAnimals()) {
                     System.out.println("ID: " + a.getId() + " | Nome: " + a.getName());
                 }
-            } else if (opcao == 4) { // Emitir Som (Busca por ID) [cite: 38]
+            } else if (opcao == 4) { // Emitir Som (Busca por ID)
                 System.out.print("Digite o ID: ");
                 long idBusca = ler.nextLong();
                 for (Animal a : meuZoo.listAnimals()) {
@@ -34,7 +34,7 @@ public class Main {
                     }
                 }
             }
-            // Opção 5: Testar Habilidade [cite: 39]
+            // Opção 5: Testar Habilidade
             else if (opcao == 5) {
                 System.out.print("ID: ");
                 long idHab = ler.nextLong();
@@ -46,6 +46,7 @@ public class Main {
                     }
                 }
             }
+            ler.close();
         }
     }
 }
